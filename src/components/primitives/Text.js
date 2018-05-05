61a10702cc0583a0ee4291eaab44d7d0960177b2
Greg Westneat <gwestneat@neat-designs.com>
2018-05-05 14:44:44 -0700

    Added functionality to Text

--- src/components/primitives/Text.js
+++ src/components/primitives/Text.js
@@ -8,15 +8,54 @@ type Props = {
   rest?: any,
 };
 
+const TEXT_COLOR = "#242424";
+
+const FONT_FAMILIES = {
+  SANS_SERIF: "sans-serif",
+  SERIF: "serif",
+};
+
 class Text extends Component<Props> {
   static defaultProps = {
     style: {},
     rest: {},
   };
 
+  getFontFamily = () => {
+    return { fontFamily: FONT_FAMILIES.SANS_SERIF };
+  };
+
+  getFluidSize = () => 17;
+
+  getFontSize = () => {
+    return { fontSize: this.getFluidSize() };
+  };
+
+  getLineHeight = () => {
+    return { lineHeight: 1.1 * this.getFluidSize() };
+  };
+
+  getLetterSpacing = () => {
+    // return { letterSpacing: 0 };
+    return {};
+  };
+
+  getDynamicStyles = () => {
+    return {
+      ...this.getFontFamily(),
+      ...this.getFontSize(),
+      ...this.getLineHeight(),
+      ...this.getLetterSpacing(),
+    };
+  };
+
+  // getStyles = () => {}
+
   render() {
     const { style, ...rest } = this.props;
-    return <RNText style={[styles.text, style]} {...rest} />;
+    return (
+      <RNText style={[styles.text, this.getDynamicStyles(), style]} {...rest} />
+    );
   }
 }
 
@@ -24,6 +63,6 @@ export default Text;
 
 const styles = StyleSheet.create({
   text: {
-    color: "black",
+    color: TEXT_COLOR,
   },
 });
