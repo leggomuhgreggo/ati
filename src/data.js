7bf2048d3bebddbbc60df5e93d7723923865d2fd
Greg Westneat <gwestneat@neat-designs.com>
2018-05-06 08:51:48 -0700

    moved data to separate file

---
+++ src/data.js
@@ -0,0 +1,44 @@
+import { POST_CATEGORIES } from "./constants";
+
+const DATA = [
+  {
+    title:
+      "Inside The Darvaza Gas Crater, Turkmenistanian’s Fiery Door To Hell",
+    category: POST_CATEGORIES.MYSTERY,
+    uri: "#",
+    imageSrc: "http://fillmurray.com/1397/1091",
+    id: "12",
+  },
+  {
+    title:
+      "Holly Bobo Met A Stranger For A Midnight Rendezvous, And Never Returned",
+    category: POST_CATEGORIES.MYSTERY,
+    uri: "#",
+    imageSrc: "http://fillmurray.com/100/100",
+    id: "123",
+  },
+  {
+    title:
+      "Meet Pedro Rodrigues Filho, The Real-Life “Dexter” — Serial Killer Of Other Criminals",
+    category: POST_CATEGORIES.WEIRD,
+    uri: "#",
+    imageSrc: "http://fillmurray.com/100/100",
+    id: "123asfas4",
+  },
+  {
+    title: "Bad News Alert: Bacon Will Kill You As Fast As Cigarettes",
+    category: POST_CATEGORIES.BAD_NEWS,
+    uri: "#",
+    imageSrc: "http://fillmurray.com/100/100",
+    id: "12asdf34",
+  },
+  {
+    title: "21 Photos Of André The Giant You Won’t Believe Aren’t Photoshopped",
+    category: POST_CATEGORIES.BELIEVE_IT,
+    uri: "#",
+    imageSrc: "http://fillmurray.com/100/100",
+    id: "1affeg234",
+  },
+];
+
+export default DATA;
