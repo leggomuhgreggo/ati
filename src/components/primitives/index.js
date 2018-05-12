51f75e295bbf66ba1c517c5fe9e6fb5635adc44a
Greg Westneat <gwestneat@neat-designs.com>
2018-05-11 23:26:57 -0700

    update primitives index

--- src/components/primitives/index.js
+++ src/components/primitives/index.js
@@ -1,5 +1,7 @@
+export { default as Text } from "./Text";
+export { default as View } from "./View";
 export { default as Container } from "./Container";
 export { default as Image } from "./Image";
-export { default as Text } from "./Text";
 export { default as Ratio } from "./Ratio";
-export { default as Row } from "./Row";
+export { default as FullWidthSection } from "./FullWidthSection";
+export { default as Grid } from "./Grid/Grid";
