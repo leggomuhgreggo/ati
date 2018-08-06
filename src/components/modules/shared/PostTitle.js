5e51b032011f95f2173fb6e1a45a25dd20d41da8
Greg Westneat <gwestneat@neat-designs.com>
2018-08-06 06:36:10 -0700

    Fixed PostTitle

--- src/components/modules/shared/PostTitle.js
+++ src/components/modules/shared/PostTitle.js
@@ -4,6 +4,8 @@ import React, { PureComponent } from "react";
 import { StyleSheet } from "react-native";
 
 import { Text } from "components/primitives";
+import { Responsive } from "components/utils";
+import { BREAKPOINTS } from "constants/index.js";
 
 type TitleProps = {
   title: string,
@@ -12,7 +14,23 @@ type TitleProps = {
 class PostTitle extends PureComponent<TitleProps> {
   render() {
     const { title, style } = this.props;
-    return <Text style={[styles.text, style]}>{title}</Text>;
+    return (
+      <Responsive>
+        {({ minWidth }) => {
+          const isDesktop = minWidth(BREAKPOINTS.LG);
+          const fontStyles = isDesktop
+            ? {
+                fontSize: 18,
+                lineHeight: 23,
+              }
+            : {
+                fontSize: 15,
+                lineHeight: 20,
+              };
+          return <Text style={[styles.text, fontStyles, style]}>{title}</Text>;
+        }}
+      </Responsive>
+    );
   }
 }
 
