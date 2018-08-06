c6e61e6a4d7a2f2bbd56c3816b76a60415b36825
Greg Westneat <gwestneat@neat-designs.com>
2018-08-06 00:36:33 -0700

    cleaned up page factory

--- src/PageFactory.js
+++ src/PageFactory.js
@@ -1,19 +1,14 @@
 // @flow
 
-// @flow
-
 import React, { PureComponent, Fragment } from "react";
-
 import {
   MostRecent,
   TagPostsSingleColumn,
   Instagram,
   Newsletter,
   Trending,
-} from "./components/modules";
-
-import { ResponsiveLeaderboard as Leaderboard } from "./components/ads";
-
+} from "components/modules";
+import { ResponsiveLeaderboard } from "components/ads";
 import { Section } from "components/primitives";
 
 import {
@@ -21,6 +16,45 @@ import {
   SMALL_SECTIONS,
   SECTION_SPACING_VARIANTS,
 } from "constants/index";
+
+export const SECTION_TYPE_COMPONENT_MAP = {
+  [SECTION_TYPES.AD]: ResponsiveLeaderboard,
+  [SECTION_TYPES.RECENT]: MostRecent,
+  [SECTION_TYPES.TAG_TILE_BOX]: TagPostsSingleColumn,
+  [SECTION_TYPES.INSTAGRAM]: Instagram,
+  [SECTION_TYPES.NEWSLETTER]: Newsletter,
+  [SECTION_TYPES.TRENDING]: Trending,
+};
+
+const getSectionID = ({ type, id }) => `${type}-${id}`;
+class PageSections extends PureComponent {
+  render() {
+    const { data } = this.props;
+    return (
+      <Fragment>
+        {data.map((item, index, array) => {
+          const spacingVariant = getSequenceAwareSpacingVariant(
+            item,
+            index,
+            array,
+          );
+
+          const id = getSectionID(item);
+
+          const Module = getSectionComponentNameBySectionType(item.type);
+
+          return (
+            <Section id={id} key={id} topSpacing={spacingVariant}>
+              <Module {...item} />
+            </Section>
+          );
+        })}
+      </Fragment>
+    );
+  }
+}
+
+export default PageSections;
 
 export const isSmallType = ({ type }) => SMALL_SECTIONS.includes(type);
 
@@ -40,41 +74,5 @@ export const getSequenceAwareSpacingVariant = (
     : SECTION_SPACING_VARIANTS.LARGE;
 };
 
-export const SECTION_TYPE_COMPONENT_MAP = {
-  [SECTION_TYPES.AD]: Leaderboard,
-  [SECTION_TYPES.RECENT]: MostRecent,
-  [SECTION_TYPES.TAG_TILE_BOX]: TagPostsSingleColumn,
-  [SECTION_TYPES.INSTAGRAM]: Instagram,
-  [SECTION_TYPES.NEWSLETTER]: Newsletter,
-  [SECTION_TYPES.TRENDING]: Trending,
-};
-
 export const getSectionComponentNameBySectionType = sectionType =>
   SECTION_TYPE_COMPONENT_MAP[sectionType];
-
-class PageSections extends PureComponent {
-  render() {
-    const { data } = this.props;
-    return (
-      <Fragment>
-        {data.map((item, index, array) => {
-          const spacingVariant = getSequenceAwareSpacingVariant(
-            item,
-            index,
-            array,
-          );
-          const Module = getSectionComponentNameBySectionType(item.type);
-          const id = `${item.type}-${item.moduleId}`;
-
-          return (
-            <Section id={id} key={id} topSpacing={spacingVariant}>
-              <Module {...item} />
-            </Section>
-          );
-        })}
-      </Fragment>
-    );
-  }
-}
-
-export default PageSections;
