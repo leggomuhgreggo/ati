baa5757db1a7d2f8868af0b49fd2bdf8e64ff561
Greg Westneat <gwestneat@neat-designs.com>
2018-08-06 00:37:11 -0700

    reduced API of Text

--- src/components/primitives/Text/Text.js
+++ src/components/primitives/Text/Text.js
@@ -1,94 +1,33 @@
 // @flow
 
 import React, { PureComponent } from "react";
-import { Text as RNText, StyleSheet, Dimensions } from "react-native";
-
-import createLockFunction from "utils/lock";
+import { Text as RNText, StyleSheet } from "react-native";
 
 import type { StyleObj } from "react-native/Libraries/StyleSheet/StyleSheetTypes";
 
-const TEXT_COLOR = "#242424";
-
-const FONT_FAMILIES = {
-  SANS_SERIF: "Work Sans",
-  SERIF: "Libre Baskerville",
-};
+import { FONT_FAMILIES, COLOR_MAP } from "constants/index";
 
 type Props = {
   style?: StyleObj,
-  size?: 1 | 2 | 3 | 4 | 5 | 6,
-  sizeRange?: [number, number],
   serif?: boolean,
   sansSerif?: boolean,
-  screenWidth?: number,
 };
 
 class Text extends PureComponent<Props> {
   static defaultProps = {
     style: {},
     rest: {},
-    size: 1,
-    sizeRange: [17, 21],
-    screenWidth: Dimensions.get("window").width,
     serif: false,
     sansSerif: true,
   };
 
-  getValueFromStyleProp = cssProperty =>
-    !!StyleSheet.flatten(this.props.style)[cssProperty] &&
-    StyleSheet.flatten(this.props.style)[cssProperty];
-
-  getFontFamily = () =>
-    this.props.serif ? FONT_FAMILIES.SERIF : FONT_FAMILIES.SANS_SERIF;
-
-  getFontSizeFromLockFunc = () => {
-    const {
-      screenWidth,
-      size,
-      sizeRange: [min, max],
-    } = this.props;
-
-    const sizeMod = Math.pow(0.9, size - 1) * size;
-
-    return createLockFunction({ min, max })(screenWidth) * sizeMod;
-  };
-
-  getFontSize = () =>
-    this.getValueFromStyleProp("fontSize") || this.getFontSizeFromLockFunc();
-
-  //
-  getLineHeightCoordinates = () => {
-    return {
-      min: 1.25,
-      max: 1.1,
-      lockMin: 16,
-      lockMax: 30,
-    };
-  };
-
-  getLineHeightFromLockFunc = () => {
-    const fontSize = this.getFontSize();
-    const lockConfig = this.getLineHeightCoordinates();
-    return createLockFunction(lockConfig)(fontSize);
-  };
-
-  getLineHeight = () =>
-    this.getValueFromStyleProp("lineHeight") ||
-    this.getLineHeightFromLockFunc();
-
-  getDynamicStyles = () => {
-    // console.log(this.getLineHeight(), this.getFontSize());
-    return {
-      fontFamily: this.getFontFamily(),
-      fontSize: ~~this.getFontSize(),
-      lineHeight: ~~(this.getLineHeight() * this.getFontSize()),
-    };
-  };
-
   render() {
-    const { style, children } = this.props;
+    const { serif, sansSerif, style, children, ...rest } = this.props;
+    const fontFamily = this.props.serif
+      ? FONT_FAMILIES.SERIF
+      : FONT_FAMILIES.SANS_SERIF;
     return (
-      <RNText style={[styles.text, this.getDynamicStyles(), style]}>
+      <RNText {...rest} style={[styles.text, { fontFamily }, style]}>
         {children}
       </RNText>
     );
@@ -99,7 +38,6 @@ export default Text;
 
 const styles = StyleSheet.create({
   text: {
-    color: TEXT_COLOR,
-    fontFamily: "sans-serif",
+    color: COLOR_MAP.TEXT_COLOR,
   },
 });
