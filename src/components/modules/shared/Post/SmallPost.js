8ce35f2970cdb70e035598309ada0a812e99a031
Greg Westneat <gwestneat@neat-designs.com>
2018-09-26 21:55:57 -0700

    conditon link props for small post

--- src/components/modules/shared/Post/SmallPost.js
+++ src/components/modules/shared/Post/SmallPost.js
@@ -31,8 +31,9 @@ class SmallPost extends PureComponent<Props> {
     const fontStyles = isDesktop
       ? { fontSize: 17, lineHeight: 24 }
       : { fontSize: 15, lineHeight: 20 };
+    const linkProps = link ? { accessibilityRole: "link", href: link } : {};
     return (
-      <View style={[styles.postBox]} accessibilityRole="link" href={link}>
+      <View style={[styles.postBox]} {...linkProps}>
         <PostLabel categoryName={categoryName} categoryColor={categoryColor} />
         <View style={{ marginTop: 6 }}>
           <Text
