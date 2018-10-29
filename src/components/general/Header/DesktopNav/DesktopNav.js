5236c4e0c2f51807990639bab675a1bae4a45cac
Greg Westneat <gwestneat@neat-designs.com>
2018-10-28 17:21:52 -0700

    Started writing desktop nav

---
+++ src/components/general/Header/DesktopNav/DesktopNav.js
@@ -0,0 +1,46 @@
+import React, { PureComponent } from "react";
+import { View, StyleSheet } from "react-native";
+import { Text } from "components/primitives";
+import NavBar from "../NavBar";
+import { TAG_LINKS } from "../nav-constants";
+
+export default class DesktopNav extends PureComponent {
+  render() {
+    const { onMenuButtonClick } = this.props;
+    return (
+      <NavBar
+        renderRight={() => <AtiLogo />}
+        renderCenter={() => <TagLinks links={TAG_LINKS} />}
+        renderLeft={() => <SocialLinks />}
+      />
+    );
+  }
+}
+
+const TagLinks = ({ links }) => {
+  return (
+    <View style={[styles.tagLinks]}>
+      {links.map(({ text, href }) => (
+        <Text style={[styles.headerText, styles.linkItem]} href={href}>
+          {text}
+        </Text>
+      ))}
+    </View>
+  );
+};
+
+const AtiLogo = () => <Text style={[styles.headerText]}>ATI</Text>;
+
+const SocialLinks = () => <Text style={[styles.headerText]}>SOCIAL</Text>;
+
+const styles = StyleSheet.create({
+  tagLinks: {
+    flexDirection: "row",
+  },
+  linkItem: {
+    paddingHorizontal: 10,
+  },
+  headerText: {
+    color: "white",
+  },
+});
--- src/components/general/Header/Header.js
+++ src/components/general/Header/Header.js
@@ -7,19 +7,15 @@ import { Responsive } from "components/utils";
 import { Text } from "components/primitives";
 
 import MobileNav from "./MobileNav/MobileNav";
+import DesktopNav from "./DesktopNav/DesktopNav";
 
 import { BREAKPOINTS, HEADER_HEIGHT } from "constants/index.js";
 
+const HEADER_Z_INDEX = 999;
+
 type Props = {};
-type State = {
-  drawerOpen: boolean,
-};
 
-class Header extends PureComponent<Props, State> {
-  state = {
-    drawerOpen: false,
-  };
-
+class Header extends PureComponent<Props> {
   render() {
     return (
       <Responsive>
@@ -28,13 +24,7 @@ class Header extends PureComponent<Props, State> {
 
           return (
             <View style={[styles.headerWrap]}>
-              {isDesktop ? (
-                <View style={[styles.headerBar]}>
-                  <Text style={[styles.headerText]}>ATI</Text>
-                </View>
-              ) : (
-                <MobileNav />
-              )}
+              {isDesktop ? <DesktopNav /> : <MobileNav />}
             </View>
           );
         }}
@@ -47,33 +37,6 @@ export default Header;
 
 const styles = StyleSheet.create({
   headerWrap: {
-    zIndex: 999,
+    zIndex: HEADER_Z_INDEX,
   },
-  headerBar: {
-    width: "100%",
-    height: HEADER_HEIGHT,
-    backgroundColor: "black",
-    flexDirection: "row",
-    justifyContent: "space-between",
-    alignItems: "center",
-    paddingHorizontal: 15,
-    position: "relative",
-    zIndex: 1,
-  },
-  headerText: {
-    color: "white",
-  },
-  inner: {
-    backgroundColor: "white",
-    padding: 15,
-    flex: 1,
-    justifyContent: "space-between",
-  },
-  linkGroup: {
-    paddingVertical: 10,
-  },
-  linkGroupsWrap: {},
-  link: {},
-  icon: {},
-  reportWrap: {},
 });
