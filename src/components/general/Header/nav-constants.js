f436ae315e34ee381d231cb27de636afa15dbe50
Greg Westneat <gwestneat@neat-designs.com>
2018-10-28 18:22:36 -0700

    Improve social links types

--- src/components/general/Header/nav-constants.js
+++ src/components/general/Header/nav-constants.js
@@ -5,6 +5,13 @@ import {
   FaPinterest,
 } from "react-icons/fa";
 
+export const SOCIAL_TYPES = {
+  FACEBOOK: "facebook",
+  TWITTER: "twitter",
+  PINTEREST: "pinterest",
+  NEWSLETTER: "newsletter",
+};
+
 export const TAG_LINKS = [
   {
     href: "/tag/history/",
@@ -22,21 +29,25 @@ export const TAG_LINKS = [
 
 export const SOCIAL_LINKS = [
   {
+    type: SOCIAL_TYPES.FACEBOOK,
     href: "https://www.facebook.com/allthatsinteresting",
-    text: "facebook",
+    text: "Facebook",
     iconComponent: FaFacebookF,
   },
   {
+    type: SOCIAL_TYPES.TWITTER,
     href: "https://twitter.com/ATInteresting",
-    text: "twitter",
+    text: "Twitter",
     iconComponent: FaTwitter,
   },
   {
+    type: SOCIAL_TYPES.PINTEREST,
     href: "https://www.pinterest.com/allthatisintere/",
-    text: "pinterest",
+    text: "Pinterest",
     iconComponent: FaPinterest,
   },
   {
+    type: SOCIAL_TYPES.NEWSLETTER,
     href: "http://interestin.gg/AN2pn3",
     text: "Newsletter",
     iconComponent: FaEnvelope,
