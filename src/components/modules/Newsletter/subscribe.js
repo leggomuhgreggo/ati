3f83183cade6b343eb28a4e75dde312d25b8dfa4
Greg Westneat <gwestneat@neat-designs.com>
2019-02-11 18:20:11 -0800

    extract newsletter submit endpont to constant

--- src/components/modules/Newsletter/subscribe.js
+++ src/components/modules/Newsletter/subscribe.js
@@ -1,7 +1,9 @@
 // @flow
 
+const NEWSLETTER_SUBMISSION_URL = "https://pbh-network.com/subs.php?json=true";
+
 export function subscribe(email: string) {
-  return fetch("https://pbh-network.com/subs.php?json=true", {
+  return fetch(NEWSLETTER_SUBMISSION_URL, {
     method: "POST",
     body: getSubscribeBody(email),
   });
