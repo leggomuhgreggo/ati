b1e8f605eea857fd007aa27c6200ca7f16bf7d90
Greg Westneat <gwestneat@neat-designs.com>
2019-02-11 18:19:01 -0800

    fix param casing for submit req

--- src/components/modules/Newsletter/subscribe.js
+++ src/components/modules/Newsletter/subscribe.js
@@ -3,6 +3,10 @@
 export function subscribe(email: string) {
   return fetch("https://pbh-network.com/subs.php?json=true", {
     method: "POST",
-    body: new URLSearchParams({ email }),
+    body: getSubscribeBody(email),
   });
 }
+
+const getSubscribeBody = email => {
+  return new URLSearchParams({ EMAIL: email });
+};
